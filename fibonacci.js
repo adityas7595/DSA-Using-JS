d3ed1adfee424557f8b7c3e40d0eadf15f449f82
Aditya Singh <adityack1234@gmail.com>
2024-06-02 12:39:00 +0530

    This is file for fibonacci series in js

---
+++ fibonacci.js
@@ -0,0 +1,18 @@
+const fibonacci = (n) =>{
+    if(n === 0) return [0];
+    if( n === 1) return [1];
+    let fib =  [0,1];
+    for(let i=2; i<n; i++){
+      fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib;
+  }
+  
+  // O(n^2) 
+  
+  console.log(fibonacci(0));
+  console.log(fibonacci(1));
+  console.log(fibonacci(2));
+  console.log(fibonacci(3));
+  console.log(fibonacci(4));
+  console.log(fibonacci(5));
